801d2113d7ef3c1f86791771c1a132f6bd8ea190
bernard rayoso <rayosobernard@gmail.com>
2024-04-14 22:30:01 +0800

    implement clerk signin and signup feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "@/providers/theme-provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
-        <ThemeProvider>{children}</ThemeProvider>
-      </body>
+      <ClerkProvider>
+        <body className={inter.className}>
+          <ThemeProvider>{children}</ThemeProvider>
+        </body>
+      </ClerkProvider>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
 import { Button, Input } from "antd";
 
 export default function Home() {
   return (
     <div className='flex flex-col items-center justify-center h-screen gap-5'>
       <h1 className='text-3xl text-gray-500 font-bold'>Hotely</h1>
-      <Button type='primary'>Get Started</Button>
-      <Button type='default'>Get Started</Button>
-      <Button type='link'>Get Started</Button>
-      <Input placeholder='username' />
+      <UserButton afterSignOutUrl="/sign-in"/>
     </div>
   );
 }
