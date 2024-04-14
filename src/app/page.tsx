4a51d1f377d17e541960e9bc49254512bd99ee2a
bernard rayoso <rayosobernard@gmail.com>
2024-04-15 00:31:21 +0800

    Show currentUser details on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, currentUser } from "@clerk/nextjs";
 import { Button, Input } from "antd";
 
-export default function Home() {
+export default async function Home() {
+  let clerkUserId = "";
+  let name = "";
+  let email = "";
+
+  const currentUserData = await currentUser();
+  console.log(currentUserData);
+  if (currentUserData) {
+    clerkUserId = currentUserData.id;
+    name = currentUserData.firstName + " " + currentUserData.lastName;
+    email = currentUserData.emailAddresses[0].emailAddress;
+  }
+
   return (
     <div className='flex flex-col items-center justify-center h-screen gap-5'>
       <h1 className='text-3xl text-gray-500 font-bold'>Hotely</h1>
-      <UserButton afterSignOutUrl="/sign-in"/>
+      <h1>Cler user ID : {clerkUserId}</h1>
+      <h1>Name: {name}</h1>
+      <h1>Email : {email}</h1>
+      <UserButton afterSignOutUrl='/sign-in' />
     </div>
   );
 }
