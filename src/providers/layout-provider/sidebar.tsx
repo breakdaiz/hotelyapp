9092916573ef1dbf38ea23ac6e725105762de0b5
bernard rayoso <rayosobernard@gmail.com>
2024-04-18 00:30:50 +0800

    Add menu items for normal user and admin

--- src/providers/layout-provider/sidebar.tsx
+++ src/providers/layout-provider/sidebar.tsx
@@ -1,19 +1,89 @@
+import { UserType } from "@/interfaces";
 import { Drawer } from "antd";
+import { BedDouble, GitGraph, Home, List, User } from "lucide-react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 function Sidebar({
   setShowSideBar,
   showSideBar,
+  loggedInUserData,
 }: {
   showSideBar: boolean;
   setShowSideBar: React.Dispatch<React.SetStateAction<boolean>>;
+  loggedInUserData: UserType;
 }) {
+  const iconSize = 18;
+  const router = useRouter();
+
+  const userMenuItems: any[] = [
+    {
+      name: "Home",
+      icon: <Home size={iconSize} />,
+      onClick: () => router.push("/"),
+    },
+    {
+      name: "Bookings",
+      icon: <List size={iconSize} />,
+      onClick: () => router.push("/user/bookings"),
+    },
+    {
+      name: "Profile",
+      icon: <User size={iconSize} />,
+      onClick: () => router.push("/user/profile"),
+    },
+  ];
+  const adminMenutems: any[] = [
+    {
+      name: "Home",
+      icon: <Home size={iconSize} />,
+      onClick: () => router.push("/"),
+    },
+    {
+      name: "Bookings",
+      icon: <List size={iconSize} />,
+      onClick: () => router.push("/admin/bookings"),
+    },
+    {
+      name: "Hotels",
+      icon: <User size={iconSize} />,
+      onClick: () => router.push("/admin/hotels"),
+    },
+    {
+      name: "Rooms",
+      icon: <BedDouble size={iconSize} />,
+      onClick: () => router.push("/admin/rooms"),
+    },
+    {
+      name: "Reports",
+      icon: <GitGraph size={iconSize} />,
+      onClick: () => router.push("/admin/reports"),
+    },
+  ];
+
+  const menuItems: any[] = loggedInUserData.isAdmin
+    ? adminMenutems
+    : userMenuItems;
+
   return (
-    <Drawer
-      open={showSideBar}
-      onClose={() => setShowSideBar(false)}
-      closable
-    ></Drawer>
+    <Drawer open={showSideBar} onClose={() => setShowSideBar(false)} closable>
+      <div className="flex flex-col gap-10"> 
+      {menuItems.map((item, index) => (
+        <div
+        className='flex gap-4 items-center text-gray-700 cursor-pointer'
+        key={index}
+        onClick={() => {
+          item.onClick();
+          setShowSideBar(false);
+        }}
+        >
+          {item.icon}
+
+          <span className="mt-[2px]">{item.name}</span>
+        </div>
+      ))}
+      </div>
+    </Drawer>
   );
 }
 
--- src/providers/layout-provider/user-infot.tsx
+++ src/providers/layout-provider/user-infot.tsx
@@ -6,16 +6,20 @@ import React, { useState } from "react";
 import Sidebar from "./sidebar";
 
 function UserInfo({ loggedInUserData }: { loggedInUserData: UserType }) {
-
   const [showSideBar, setShowSideBar] = useState(false);
   return (
     <div className='p-5  border-0 border-solid border-l flex items-center gap-5 '>
       <span className='text-gray-500 text-sm'>{loggedInUserData.name}</span>
-      <User className="text-gray-500" onClick={()=> setShowSideBar(!showSideBar)}/>
+      <User
+        className='cursor-pointer text-gray-500'
+        onClick={() => setShowSideBar(!showSideBar)}
+      />
 
-      {showSideBar && <Sidebar 
-      setShowSideBar={setShowSideBar}
-      showSideBar={showSideBar}/>}
+      {showSideBar && (
+        <Sidebar setShowSideBar={setShowSideBar} showSideBar={showSideBar}
+        loggedInUserData={loggedInUserData}
+        />
+      )}
     </div>
   );
 }
