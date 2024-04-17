bd362e97c17d2d4f68a716bd5a8b7f2f1caff5dd
bernard rayoso <rayosobernard@gmail.com>
2024-04-17 22:25:14 +0800

    refactor home page

--- src/providers/layout-provider/index.tsx
+++ src/providers/layout-provider/index.tsx
@@ -1,6 +1,34 @@
-import Header from "./header";
+"use client";
 
-function LayoutProvider({ children }: { children: React.ReactNode }) {
+import React, { ReactNode, useEffect, useState } from "react";
+import Header from "./header";
+import { GetCurrentUserFromMongoDB } from "@/server-actions/users";
+import { message } from "antd";
+import { UserType } from "@/interfaces";
+
+function LayoutProvider({ children }: { children: ReactNode }) {
+  const [loggedInUserData, setLoggedInUserData] = useState<UserType | null>(null);
+  const getUserData = async () => {
+    try {
+      const response = await GetCurrentUserFromMongoDB()
+      if (response.success) {
+        setLoggedInUserData(response.data);
+        console.log(response.data);
+      }
+      else {
+        throw new Error(response.messege);
+      }
+    } catch (error: any) {
+      message.error(error.message)
+      
+    }
+  };
+
+  useEffect(() => {
+    if (!loggedInUserData) {
+      getUserData();
+    }
+  });
   return (
     <div>
       <Header />
