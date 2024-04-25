46412ac6c9770b976e7fd5a3708cf641dfa5d7da
bernard rayoso <rayosobernard@gmail.com>
2024-04-26 00:23:46 +0800

    fixed bug of header component upon signin and signout

--- src/providers/layout-provider/index.tsx
+++ src/providers/layout-provider/index.tsx
@@ -5,30 +5,40 @@ import Header from "./header";
 import { GetCurrentUserFromMongoDB } from "@/server-actions/users";
 import { message } from "antd";
 import { UserType } from "@/interfaces";
+import { usePathname } from "next/navigation";
 
 function LayoutProvider({ children }: { children: ReactNode }) {
-  const [loggedInUserData, setLoggedInUserData] = useState<UserType | null>(null);
+  const [loggedInUserData, setLoggedInUserData] = useState<UserType | null>(
+    null
+  );
+
+  const pathName = usePathname();
+  const isAuthRoute =
+    pathName.includes("/sign-in") || pathName.includes("/sign-up");
+
   const getUserData = async () => {
     try {
-      const response = await GetCurrentUserFromMongoDB()
+      const response = await GetCurrentUserFromMongoDB();
       if (response.success) {
         setLoggedInUserData(response.data);
         console.log(response.data);
-      }
-      else {
+      } else {
         throw new Error(response.messege);
       }
     } catch (error: any) {
-      message.error(error.message)
-      
+      message.error(error.message);
     }
   };
 
   useEffect(() => {
-    if (!loggedInUserData) {
+    if (!loggedInUserData && !isAuthRoute) {
       getUserData();
     }
   });
+
+  if (isAuthRoute) {
+    return children;
+  }
   return (
     <div>
       <Header loggedInUserData={loggedInUserData} />
